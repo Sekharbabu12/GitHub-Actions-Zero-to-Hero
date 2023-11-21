55715c650b3118b7a171ce34982eb5b9156cbb02
Somasekhar Gollapalli <98102921+Sekharbabu12@users.noreply.github.com>
2023-11-21 13:55:56 +0530

    Create greeting.py

---
+++ src/greeting.py
@@ -0,0 +1,7 @@
+# Simple Python code example
+
+# Prompt the user for their name
+name = input("Somasekhar")
+
+# Print a personalized greeting
+print(f"Hello, {name}! Welcome to Python programming.")
