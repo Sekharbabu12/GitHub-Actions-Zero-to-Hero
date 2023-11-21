580ca291f402305d872a0f3ff8d2caad859fb3cd
Somasekhar Gollapalli <98102921+Sekharbabu12@users.noreply.github.com>
2023-11-21 17:01:36 +0530

    Update greeting.py

--- src/greeting.py
+++ src/greeting.py
@@ -4,3 +4,13 @@ def get_name():
 
 name = get_name()
 
+
+# test_greeting.py
+from greeting import get_name
+
+def test_get_name():
+    # You can use a testing library to mock user input
+    # or directly provide the input depending on your needs
+    assert get_name() == "Somasekhar"
+
+
