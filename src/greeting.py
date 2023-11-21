107dbea42404cc61f1eda9bbef1bc3c53c29da21
Somasekhar Gollapalli <98102921+Sekharbabu12@users.noreply.github.com>
2023-11-21 18:36:05 +0530

    Update greeting.py

--- src/greeting.py
+++ src/greeting.py
@@ -5,12 +5,13 @@ def get_name():
 name = get_name()
 
 
-# test_greeting.py
-from greeting import get_name
+# greeting.py
+def get_name(input_func=input):
+    return input_func("Enter your name: ")
 
-def test_get_name():
-    # You can use a testing library to mock user input
-    # or directly provide the input depending on your needs
-    assert get_name() == "Somasekhar"
+if __name__ == "__main__":
+    name = get_name()
+    print(f"Hello, {name}!")
+
 
 
