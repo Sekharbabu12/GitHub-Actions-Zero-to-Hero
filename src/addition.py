c4367962673b9a4f61120feb5090c9a006a1c6d0
Somasekhar Gollapalli <98102921+Sekharbabu12@users.noreply.github.com>
2023-11-21 13:42:34 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,5 +1,6 @@
 # app.py
 # This is a test commit
+# This is addition code
 def add(a, b):
     return a + b
 
@@ -7,10 +8,3 @@ def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
 
-# subtraction code 
-def sub(a, b):
-    return a - b
-
-def test_add():
-    assert sub(1, 2) == -1
-    assert sub(1, -1) == 2
